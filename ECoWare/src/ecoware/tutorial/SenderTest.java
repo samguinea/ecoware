f2fc74ea38744d822a90d29460c55d513c6363f3
avarriale <armando.varriale@gmail.com>
2013-07-16 03:08:09 +0200

    Update

--- ECoWare/src/ecoware/tutorial/SenderTest.java
+++ ECoWare/src/ecoware/tutorial/SenderTest.java
@@ -16,17 +16,27 @@ public class SenderTest {
 		System.out.println("Producer started...");
 		HashMap<String, Object> mapMsg = new HashMap<String, Object>();
 		ECoWareMessageSender sender;
+		int msgs = 100;
 		try {
 			sender = new ECoWareMessageSender("localhost", "BrowserInfo");
 			sender.startConnection();
-			while(true){
+			for(int i=0; i<msgs; i++){
 				mapMsg.put("key", "105");
 				mapMsg.put("value", 1.0);
 				sender.send(mapMsg, ECoWareEventType.START_TIME, -1);
 				mapMsg.put("key", "105");
 				mapMsg.put("value", 3.0);
 				sender.send(mapMsg, ECoWareEventType.END_TIME, -1);
+				Thread.sleep(10);
+				mapMsg.put("key", "105");
+				mapMsg.put("value", 2.0);
+				sender.send(mapMsg, ECoWareEventType.START_TIME, -1);
+				mapMsg.put("key", "105");
+				mapMsg.put("value", 3.5);
+				sender.send(mapMsg, ECoWareEventType.END_TIME, -1);
+				Thread.sleep(100);
 			}
+			sender.stopConnection();
 		} catch (IOException e1) {
 			e1.printStackTrace();
 		} catch (Exception e1) {
